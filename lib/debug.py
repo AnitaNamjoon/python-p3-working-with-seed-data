2bcd2c2eb0b8eadbfea8bc9fc2f57e3d341a7605
AnitaNamjoon <135642350+AnitaNamjoon@users.noreply.github.com>
2023-09-05 09:09:57 +0300

    Update debug.py

--- lib/debug.py
+++ lib/debug.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env python3
 
 from faker import Faker
@@ -10,10 +11,24 @@ from models import Game
 fake = Faker()
 
 if __name__ == '__main__':
-    
-    engine = create_engine('sqlite:///seed_db.db')
+    # Create an SQLAlchemy engine and session
+    engine = create_engine('sqlite:///seed_db.db')  # Replace with your database URL
     Session = sessionmaker(bind=engine)
     session = Session()
 
+    print("Seeding games...")
 
-    import ipdb; ipdb.set_trace()
+    games = [
+        Game(
+            title=fake.name(),
+            genre=fake.word(),
+            platform=fake.word(),
+            price=random.randint(0, 60)
+        )
+        for i in range(50)
+    ]
+
+    session.bulk_save_objects(games)
+    session.commit()
+
+    print("Seed data added successfully.")
