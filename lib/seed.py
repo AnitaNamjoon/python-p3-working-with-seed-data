6d286e5cfbdadf52e87259cc5cf252060e55f8e7
AnitaNamjoon <135642350+AnitaNamjoon@users.noreply.github.com>
2023-09-05 09:09:19 +0300

    Update seed.py

--- lib/seed.py
+++ lib/seed.py
@@ -10,7 +10,16 @@ from models import Game
 fake = Faker()
 
 if __name__ == '__main__':
-    
-    engine = create_engine('sqlite:///seed_db.db')
+    # Create an SQLAlchemy engine and session
+    engine = create_engine('sqlite:///seed_db.db')  # Replace with your database URL
     Session = sessionmaker(bind=engine)
     session = Session()
+
+    botw = Game(title="Breath of the Wild", platform="Switch", genre="Adventure", price=60)
+    ffvii = Game(title="Final Fantasy VII", platform="Playstation", genre="RPG", price=30)
+    mk8 = Game(title="Mario Kart 8", platform="Switch", genre="Racing", price=50)
+
+    session.bulk_save_objects([botw, ffvii, mk8])
+    session.commit()
+
+    print("Seed data added successfully.")
